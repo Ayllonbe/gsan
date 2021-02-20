73cacb5bc8586c20000934341cf45a2c02554868
Ayllonbe <ayllonbenitez.aaron@gmail.com>
2021-02-20 08:01:40 +0100

    adding dependencies for GSAnREST

--- src/main/java/gsan/server/gsan/api/GSAnRESTController.java
+++ src/main/java/gsan/server/gsan/api/GSAnRESTController.java
@@ -36,6 +36,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
